48c75cd3bd01e5a84e989ba275c146dc9255a9a0
Kemi Chris <ekeminichristopher123@gmail.com>
2023-04-25 16:41:32 +0100

    script updated

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,3 +1,38 @@
+// script for the nav bar 
+const menuIcon = document.getElementById("nav-icon");
+const navMenuItems = document.getElementById("nav-menu-items");
+const closeMobileMenu = document.getElementById("menu-close");
+const closeSearchBar = document.getElementById("close-search-bar");
+const openSearchBar = document.getElementById("open-search-bar");
+const searchBar = document.querySelector(".search-bar");
+const mobileMenu = document.querySelector(".mobile-menu");
+
+// opening the search bar 
+openSearchBar.addEventListener("click", ()=> {
+    navMenuItems.classList.add("active");
+    searchBar.classList.remove("active");
+});
+
+// closing the search bar 
+closeSearchBar.addEventListener("click", ()=> {
+    searchBar.classList.add("active");
+    navMenuItems.classList.remove("active");
+});
+
+// opening the menu dropdown 
+menuIcon.addEventListener("click", ()=> {
+    navMenuItems.classList.add("active");
+    mobileMenu.classList.remove("active");
+});
+
+// closing the menu dropdown
+closeMobileMenu.addEventListener("click", ()=> {
+    mobileMenu.classList.add("active");
+    navMenuItems.classList.remove("active");
+});
+
+
+
 // script for the top container carousel section 
 const nextEl = document.querySelector(".next");
 const prevEl = document.querySelector(".prev");
@@ -59,3 +94,5 @@ function Slideroll(){
 }
 
 Slideroll();
+
+
