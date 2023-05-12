0e0c6802a886990a81f69fa9d65ffd32966c7fde
Kemi Chris <ekeminichristopher123@gmail.com>
2023-05-12 22:31:32 +0100

    select page responsive

--- Scripts/main.js
+++ Scripts/main.js
@@ -51,41 +51,41 @@ categoryCollapse.addEventListener("click", ()=> {
 
 
 // script for the top container carousel section 
-const nextEl = document.querySelector(".next");
-const prevEl = document.querySelector(".prev");
-const slideContainerEl = document.querySelector(".slider");
-const slides = document.querySelectorAll(".carousel-slides");
+// const nextEl = document.querySelector(".next");
+// const prevEl = document.querySelector(".prev");
+// const slideContainerEl = document.querySelector(".slider");
+// const slides = document.querySelectorAll(".carousel-slides");
 
-let currentSlide = 1
-let timeout;
+// let currentSlide = 1
+// let timeout;
 
-nextEl.addEventListener("click", ()=>{
-    currentSlide++;
-    clearTimeout(timeout);
-    updateSlide();
-});
+// nextEl.addEventListener("click", ()=>{
+//     currentSlide++;
+//     clearTimeout(timeout);
+//     updateSlide();
+// });
 
-prevEl.addEventListener("click", ()=>{
-    currentSlide--;
-    clearTimeout(timeout);
-    updateSlide();
-})
+// prevEl.addEventListener("click", ()=>{
+//     currentSlide--;
+//     clearTimeout(timeout);
+//     updateSlide();
+// })
 
 
-function updateSlide(){
-    if (currentSlide > slides.length) {
-        currentSlide = 1
-    }else if (currentSlide < 1) {
-        currentSlide = slides.length;
-    }
-    slideContainerEl.style.transform = `translateX(-${(currentSlide -1) * 25.813}rem)`
+// function updateSlide(){
+//     if (currentSlide > slides.length) {
+//         currentSlide = 1
+//     }else if (currentSlide < 1) {
+//         currentSlide = slides.length;
+//     }
+//     slideContainerEl.style.transform = `translateX(-${(currentSlide -1) * 25.813}rem)`
 
-    timeout = setTimeout(() => {
-        currentSlide++;
-        updateSlide();
-    }, 3000);
+//     timeout = setTimeout(() => {
+//         currentSlide++;
+//         updateSlide();
+//     }, 3000);
 
-}
+// }
 
 // script for the review section 
 
